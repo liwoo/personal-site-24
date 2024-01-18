badffe315deb50367f3d3f8e1af44b28c194222b
liwoo <jeremiahchienda@gmail.com>
2024-01-19 00:32:17 +0200

    added tweet theme switching

--- src/components/widgets/Tweet.tsx
+++ src/components/widgets/Tweet.tsx
@@ -1,17 +1,14 @@
-import React from 'react'
+import React from 'react';
 import { TwitterTweetEmbed } from 'react-twitter-embed';
 
 interface Props {
-    tweetId: string;
+  tweetId: string;
 }
 
 export default function Tweet({ tweetId }: Props) {
   return (
     <div className="container-article my-8 border-t border-muted py-8">
-  <TwitterTweetEmbed 
-    placeholder={"Loading Tweet"}
-    options={{theme: 'dark'}}
-    tweetId={tweetId} />
-</div>
-  )
+      <TwitterTweetEmbed placeholder={'Loading Tweet'} options={{ theme: 'dark' }} tweetId={tweetId} />
+    </div>
+  );
 }
