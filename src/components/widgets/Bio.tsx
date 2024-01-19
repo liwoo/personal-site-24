e05679984f72151b9f0cebdb8fe34ec8cced2a03
liwoo <jeremiahchienda@gmail.com>
2024-01-19 09:30:10 +0200

    added twitter to bio

--- src/components/widgets/Bio.tsx
+++ src/components/widgets/Bio.tsx
@@ -1,4 +1,5 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs';
+import Tweet from './Tweet';
 
 export function Bio() {
   return (
@@ -8,7 +9,7 @@ export function Bio() {
         <TabsTrigger value="brief">Brief Profile</TabsTrigger>
         <TabsTrigger value="full">Full Bio</TabsTrigger>
       </TabsList>
-      <TabsContent value="brief" className="leading-7 text-lg text-default py-4">
+      <TabsContent value="full" className="leading-7 text-lg text-default py-4">
         <div className="flex">
           <div className="w-1/4">Img Here</div>
           <div className="w-3/4 flex flex-col gap-y-4 ">
@@ -17,18 +18,51 @@ export function Bio() {
               multiple industries worldwide.
             </p>
             <p>
-              He currently works with <a href='https://irembo.com/' className='underline'>Irembo</a>, where he helps build the next generation of <a href='https://irembo.gov.rw/' className='underline'>Government Services</a> in Rwanda for more 
-              than 12M citizens.
+              He currently works with{' '}
+              <a href="https://irembo.com/" className="underline">
+                Irembo
+              </a>
+              , where he helps build the next generation of{' '}
+              <a href="https://irembo.gov.rw/" className="underline">
+                Government Services
+              </a>{' '}
+              in Rwanda for more than 12M citizens.
             </p>
             <p>
-              Before that, he had a brief sting with Danish-based <a href='https://bestseller.com/' className='underline'>Bestseller</a>, after having helped more than <a href="https://oneacrefund.org/our-impact" className='underline'>1M farmers accross 9 African countries</a> improve their agricultural yields while at <a href='https://oneacrefund.org/' className='underline'>One Acre Fund</a>
+              Before that, he had a brief sting with Danish-based{' '}
+              <a href="https://bestseller.com/" className="underline">
+                Bestseller
+              </a>
+              , after having helped more than{' '}
+              <a href="https://oneacrefund.org/our-impact" className="underline">
+                1M farmers accross 9 African countries
+              </a>{' '}
+              improve their agricultural yields while at{' '}
+              <a href="https://oneacrefund.org/" className="underline">
+                One Acre Fund
+              </a>
             </p>
             <p>
-              Having been born and raised in Malawi, Jeremiah, during his time at <a href='https://www.facebook.com/Baobabhealthtrust/' className='underline'>Baobab Health Trust</a> where he worked with the <a href='https://www.kuunika.org/' className='underline'>Ministry of Health</a> to usher in a <a href='https://zipatala.health.gov.mw/' className='underline'>National Facility Registry</a>.
+              Having been born and raised in Malawi, Jeremiah, during his time at{' '}
+              <a href="https://www.facebook.com/Baobabhealthtrust/" className="underline">
+                Baobab Health Trust
+              </a>{' '}
+              where he worked with the{' '}
+              <a href="https://www.kuunika.org/" className="underline">
+                Ministry of Health
+              </a>{' '}
+              to usher in a{' '}
+              <a href="https://zipatala.health.gov.mw/" className="underline">
+                National Facility Registry
+              </a>
+              .
             </p>
             <p>
-              Jeremiah is also co-founder of <a href='https://www.doorbell.mw/' className='underline'>Doorbell Malawi</a> which is looking to disrupt the consumer goods retail
-              industry in the country.
+              Jeremiah is also co-founder of{' '}
+              <a href="https://www.doorbell.mw/" className="underline">
+                Doorbell Malawi
+              </a>{' '}
+              which is looking to disrupt the consumer goods retail industry in the country.
             </p>
             <p>
               He holds a Masters of Engineering Degree from Doshisha University in Japan and a Bachelors of Science from
@@ -37,7 +71,7 @@ export function Bio() {
           </div>
         </div>
       </TabsContent>
-      <TabsContent value="pitch" className="leading-7 text-lg text-default flex flex-col gap-y-4 my-4">
+      <TabsContent value="brief" className="leading-7 text-lg text-default flex flex-col gap-y-4 my-4">
         <p>
           A Full Stack Engineer with 7+ years of experience leading multinational Engineering teams accross HealthTech,
           FinTech, AgriTech and GovTech in Africa.
@@ -47,13 +81,16 @@ export function Bio() {
           Language Processing.
         </p>
         <p>
-          <a href='/blog' className='underline hover:text-default'>Follow his blog</a> where he writes about his experiences as a Software Engineer, or book him at your event if you
-          need an expert to talk about a wide range of Software Engineering topics including Blockchain Technology and its practical usecases
-          today.
+          <a href="/blog" className="underline hover:text-default">
+            Follow his blog
+          </a>{' '}
+          where he writes about his experiences as a Software Engineer, or book him at your event if you need an expert
+          to talk about a wide range of Software Engineering topics including Blockchain Technology and its practical
+          usecases today.
         </p>
       </TabsContent>
-      <TabsContent value="full">
-        <h3>Full Biography Here</h3>
+      <TabsContent value="pitch">
+        <Tweet />
       </TabsContent>
     </Tabs>
   );
--- src/components/widgets/Tweet.tsx
+++ src/components/widgets/Tweet.tsx
@@ -1,14 +1,63 @@
-import React from 'react';
-import { TwitterTweetEmbed } from 'react-twitter-embed';
+import React, { useEffect } from 'react';
 
-interface Props {
-  tweetId: string;
-}
+export default function Tweet() {
+  useEffect(() => {
+    const script = document.createElement('script');
 
-export default function Tweet({ tweetId }: Props) {
+    script.src = 'https://platform.twitter.com/widgets.js';
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
-    <div className="container-article my-8 border-t border-muted py-8">
-      <TwitterTweetEmbed placeholder={'Loading Tweet'} options={{ theme: 'dark' }} tweetId={tweetId} />
-    </div>
+    <>
+      <div className="tweets dark">
+        <blockquote className="twitter-tweet" data-theme="dark">
+          <p lang="en" dir="ltr">
+            Hi 👋 I&#39;m Jeremiah. I&#39;m Mobile and{' '}
+            <a href="https://twitter.com/hashtag/AI?src=hash&ref_src=twsrc%5Etfw">#AI</a> 🪄 Lead at{' '}
+            <a href="https://twitter.com/IremboGov?ref_src=twsrc%5Etfw">@IremboGov</a> by ☀️ and create{' '}
+            <a href="https://t.co/sZ4UrAssGl">https://t.co/sZ4UrAssGl</a> by 🌙 <br />
+            <br />I love to{' '}
+            <a href="https://twitter.com/hashtag/buildinpublic?src=hash&ref_src=twsrc%5Etfw">#buildinpublic</a> and
+            share my failers and occasional wins on <a href="https://t.co/nAroIneVbE">https://t.co/nAroIneVbE</a>. Let’s
+            <a href="https://twitter.com/hashtag/connect?src=hash&ref_src=twsrc%5Etfw">#connect</a> if interested in{' '}
+            <a href="https://twitter.com/hashtag/softwareengineering?src=hash&ref_src=twsrc%5Etfw">
+              #softwareengineering
+            </a>
+            <a href="https://twitter.com/hashtag/blockchain?src=hash&ref_src=twsrc%5Etfw">#blockchain</a>
+            <a href="https://twitter.com/hashtag/startups?src=hash&ref_src=twsrc%5Etfw">#startups</a>
+          </p>
+          &mdash; Chienda (@liwucodes){' '}
+          <a href="https://twitter.com/liwucodes/status/1746224748067987458?ref_src=twsrc%5Etfw">January 13, 2024</a>
+        </blockquote>
+      </div>
+      <div className="tweets hidden light">
+        <blockquote className="twitter-tweet" data-theme="light">
+          <p lang="en" dir="ltr">
+            Hi 👋 I&#39;m Jeremiah. I&#39;m Mobile and{' '}
+            <a href="https://twitter.com/hashtag/AI?src=hash&ref_src=twsrc%5Etfw">#AI</a> 🪄 Lead at{' '}
+            <a href="https://twitter.com/IremboGov?ref_src=twsrc%5Etfw">@IremboGov</a> by ☀️ and create{' '}
+            <a href="https://t.co/sZ4UrAssGl">https://t.co/sZ4UrAssGl</a> by 🌙 <br />
+            <br />I love to{' '}
+            <a href="https://twitter.com/hashtag/buildinpublic?src=hash&ref_src=twsrc%5Etfw">#buildinpublic</a> and
+            share my failers and occasional wins on <a href="https://t.co/nAroIneVbE">https://t.co/nAroIneVbE</a>. Let’s
+            <a href="https://twitter.com/hashtag/connect?src=hash&ref_src=twsrc%5Etfw">#connect</a> if interested in{' '}
+            <a href="https://twitter.com/hashtag/softwareengineering?src=hash&ref_src=twsrc%5Etfw">
+              #softwareengineering
+            </a>
+            <a href="https://twitter.com/hashtag/blockchain?src=hash&ref_src=twsrc%5Etfw">#blockchain</a>
+            <a href="https://twitter.com/hashtag/startups?src=hash&ref_src=twsrc%5Etfw">#startups</a>
+          </p>
+          &mdash; Chienda (@liwucodes){' '}
+          <a href="https://twitter.com/liwucodes/status/1746224748067987458?ref_src=twsrc%5Etfw">January 13, 2024</a>
+        </blockquote>
+      </div>
+    </>
   );
 }
