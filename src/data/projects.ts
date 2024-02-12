dd66580b1795ac04153309325b152a60b1459677
liwoo <jeremiahchienda@gmail.com>
2024-02-12 13:54:44 +0200

    added get alinafe

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -110,4 +110,64 @@ export const projects: Project[] = [
         'We have been consistently impressed with the excellence with which Jeremiah delivers his services. His attention to detail is unparalleled, as he displays a profound understanding of our requirements.',
     },
   },
+  {
+    title: 'Get Alinafe Album Website',
+    subtitle:
+      'Official Album website of my debut album Alinafe, featuring celebrated Malawian Artists including David Kalilani, Suffix, Kim of Diamons, Kelvin Sings and more',
+    description:
+      "A digital space as dynamic and soulful as the music itself. Crafted with love in Svelte, this website is optimized for an immersive auditory experience, allowing fans to listen to, download, and engage with each track. Every element of the site reflects the album's unique branding, from the visual aesthetics to the interactive features. It's more than a website; it's an extension of the 'Alinafe' universe. Dive in for a seamless blend of technology and artistry, where every click brings you closer to the heart of the music.",
+    slug: 'get-alinafe',
+    company: {
+      name: 'Liwu',
+      logo: 'liwu',
+    },
+    image: {
+      src: 'https://res.cloudinary.com/tiyeni/image/upload/v1707737952/get-alinafe.jpg',
+      alt: 'Get Aliafe',
+    },
+    projectLink: 'https://getalinafe.com/',
+    sourceLink: 'https://github.com/liwoo/getalinafe',
+    duration: 'Jun 2020 - August 2020',
+    role: 'Creative Technologist',
+    stack: ['Svelte', 'Tailwind', 'Typescript'],
+    category: 'website',
+    methods: [
+      {
+        title: 'Interactive Listening Experience',
+        description:
+          'Designed an intuitive user interface that allows fans to seamlessly stream and engage with the album. Integrated audio players with high-quality streaming capabilities to ensure a smooth and immersive listening experience.',
+      },
+      {
+        title: 'Efficient Download Management',
+        description:
+          'Implemented a robust download management system enabling fans to easily download tracks and the entire album. Focused on a hassle-free process, ensuring quick and secure downloads while maintaining the integrity of the music files.',
+      },
+      {
+        title: 'User Engagement and Participation Features',
+        description:
+          'Incorporated interactive elements such as comment sections, social media integrations, and fan forums. These features were designed to foster a community of listeners, encouraging discussions and interactions around the album.',
+      },
+      {
+        title: 'Brand-centric Design',
+        description:
+          "Employed a design philosophy that deeply resonated with the album's branding. The website's aesthetic, from color schemes to typography, was carefully chosen to reflect the soul and story of 'Alinafe', creating a visual harmony with the music.",
+      },
+      {
+        title: 'Technical Implementation in Svelte',
+        description:
+          'Developed the website using Svelte, focusing on performance optimization and responsive design. This approach ensured a fast, efficient, and mobile-friendly user experience, catering to a diverse audience.',
+      },
+    ],
+    testimony: {
+      name: 'Timothy Mizaya',
+      job: 'Senior Pastor',
+      image: {
+        src: 'https://res.cloudinary.com/tiyeni/image/upload/v1707738634/1643962222674.jpg',
+        alt: 'Timothy Mizaya',
+      },
+      company: 'Flood Church',
+      testimony:
+        "Liwu's work on the GetAlinafe.com website was a game changer. As a manager, I've seen how crucial online presence is for artists. Liwu [Jeremiah] not only created a stunning website but also captured the essence of the 'Alinafe' album. The smooth streaming and downloading options significantly boosted our digital engagement. This website has not just been a platform, but a key player in elevating Liwu's music career. The impact on our audience reach and interaction has been phenomenal.",
+    },
+  },
 ];
