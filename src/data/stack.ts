33ae6475dc2803283b6f05eb5a3eb6628cffebdd
Jeremiah Chienda <jeremiahchienda@gmail.com>
2024-11-08 10:39:54 +0200

    added two draft articles

--- src/data/stack.ts
+++ src/data/stack.ts
@@ -1,117 +1,117 @@
-import type { TechStack } from "~/types";
+import type { TechStack } from '~/types';
 
 export const techStack: TechStack[] = [
-    {
-        title: "JavaScript",
-        logo: "js",
-        category: "Language",
-        experience: 8,
-    },
-    {
-        title: "TypeScript",
-        logo: "ts",
-        category: "Language",
-        experience: 6,
-    },
-    {
-        title: "React",
-        logo: "react",
-        category: "Framework",
-        experience: 6,
-    },
-    {
-        title: "Next JS",
-        logo: "next",
-        category: "Framework",
-        experience: 3,
-    },
-    {
-        title: "Astro",
-        logo: "astro",
-        category: "Other",
-        experience: 1,
-    },
-    {
-        title: "C#",
-        logo: "csharp",
-        category: "Language",
-        experience: 4,
-    },
-    {
-        title: "Dart",
-        logo: "dart",
-        category: "Language",
-        experience: 5,
-    },
-    {
-        title: ".NET",
-        logo: "dotnet",
-        category: "Framework",
-        experience: 4,
-    },
-    {
-        title: "Flutter",
-        logo: "flutter",
-        category: "Framework",
-        experience: 5,
-    },
-    {
-        title: "Remix JS",
-        logo: "remix",
-        category: "Other",
-        experience: 1,
-    },
-    {
-        title: "Go",
-        logo: "golang",
-        category: "Language",
-        experience: 2,
-    },
-    {
-        title: "Rust",
-        logo: "rust",
-        category: "Other",
-        experience: 1,
-    },
-    {
-        title: "Swift",
-        logo: "swift",
-        category: "Other",
-        experience: 1,
-    },
-    {
-        title: "Docker",
-        category: "Infrastructure",
-        experience: 4,
-    },
-    {
-        title: "Kubernetes",
-        category: "Infrastructure",
-        experience: 3,
-    },
-    {
-        title: "Postgres",
-        category: "Infrastructure",
-        experience: 6,
-    },
-    {
-        title: "Azure",
-        category: "Infrastructure",
-        experience: 1,
-    },
-    {
-        title: "Nest JS",
-        category: "Framework",
-        experience: 3,
-    },
-    {
-        title: "Firebase",
-        category: "Infrastructure",
-        experience: 4,
-    },
-    {
-        title: "Couchbase",
-        category: "Other",
-        experience: 2,
-    },
-]
+  {
+    title: 'JavaScript',
+    logo: 'js',
+    category: 'Language',
+    experience: 8,
+  },
+  {
+    title: 'Python',
+    logo: 'js',
+    category: 'Language',
+    experience: 2,
+  },
+  {
+    title: 'React',
+    logo: 'react',
+    category: 'Framework',
+    experience: 6,
+  },
+  {
+    title: 'Next JS',
+    logo: 'next',
+    category: 'Other',
+    experience: 3,
+  },
+  {
+    title: 'Astro',
+    logo: 'astro',
+    category: 'Other',
+    experience: 1,
+  },
+  {
+    title: 'C#',
+    logo: 'csharp',
+    category: 'Language',
+    experience: 4,
+  },
+  {
+    title: 'Dart',
+    logo: 'dart',
+    category: 'Language',
+    experience: 5,
+  },
+  {
+    title: '.NET',
+    logo: 'dotnet',
+    category: 'Framework',
+    experience: 4,
+  },
+  {
+    title: 'Flutter',
+    logo: 'flutter',
+    category: 'Framework',
+    experience: 5,
+  },
+  {
+    title: 'Remix JS',
+    logo: 'remix',
+    category: 'Framework',
+    experience: 3,
+  },
+  {
+    title: 'Go',
+    logo: 'golang',
+    category: 'Language',
+    experience: 2,
+  },
+  {
+    title: 'Rust',
+    logo: 'rust',
+    category: 'Other',
+    experience: 1,
+  },
+  {
+    title: 'Swift',
+    logo: 'swift',
+    category: 'Other',
+    experience: 1,
+  },
+  {
+    title: 'Docker',
+    category: 'Infrastructure',
+    experience: 4,
+  },
+  {
+    title: 'Kubernetes',
+    category: 'Infrastructure',
+    experience: 3,
+  },
+  {
+    title: 'Postgres',
+    category: 'Infrastructure',
+    experience: 6,
+  },
+  {
+    title: 'Azure',
+    category: 'Infrastructure',
+    experience: 1,
+  },
+  {
+    title: 'LangChain',
+    category: 'Framework',
+    experience: 1,
+  },
+  {
+    title: 'Firebase',
+    category: 'Infrastructure',
+    experience: 4,
+  },
+  {
+    title: 'Couchbase',
+    category: 'Other',
+    experience: 2,
+  },
+];
--- src/data/work-experience.ts
+++ src/data/work-experience.ts
@@ -1,40 +1,39 @@
-import type { WorkExperience } from "~/types";
+import type { WorkExperience } from '~/types';
 
 export const workExperience: Array<WorkExperience> = [
-    {
-        title: "Irembo",
-        location: "Kigali, RW",
-        position: "Senior Software Engineer",
-        duration: "Aug 2023 - Present",
-        image: "irembo",
-    },
-    {
-        title: "Bestseller",
-        location: "Aahurst, DK",
-        position: "Software Developer",
-        duration: "Dec 2022 - July 2023",
-        image: "bestseller",
-    },
-    {
-        title: "One Acre Fund",
-        location: "Kigali, RW",
-        position: "Lead Software Engineer",
-        duration: "Oct 2019 - Nov 2022",
-        image: "oaf",
-    },
-    {
-        title: "Baobab Health Trust",
-        location: "Lilongwe, MW",
-        position: "Software Architect",
-        duration: "Aug 2017 - Aug 2019",
-        image: "bht",
-    },
-    {
-        title: "NBS Bank",
-        location: "Blantyre, MW",
-        position: "Systems Developer",
-        duration: "Jun 2013 - Aug 2014",
-        image: "nbs",
-    }
-
-]
+  {
+    title: 'Irembo',
+    location: 'Kigali, RW',
+    position: 'AI Engineer',
+    duration: 'Aug 2023 - Present',
+    image: 'irembo',
+  },
+  {
+    title: 'Bestseller',
+    location: 'Aahurst, DK',
+    position: 'Software Developer',
+    duration: 'Dec 2022 - July 2023',
+    image: 'bestseller',
+  },
+  {
+    title: 'One Acre Fund',
+    location: 'Kigali, RW',
+    position: 'Lead Software Engineer',
+    duration: 'Oct 2019 - Nov 2022',
+    image: 'oaf',
+  },
+  {
+    title: 'Baobab Health Trust',
+    location: 'Lilongwe, MW',
+    position: 'Software Architect',
+    duration: 'Aug 2017 - Aug 2019',
+    image: 'bht',
+  },
+  {
+    title: 'NBS Bank',
+    location: 'Blantyre, MW',
+    position: 'Systems Developer',
+    duration: 'Jun 2013 - Aug 2014',
+    image: 'nbs',
+  },
+];
