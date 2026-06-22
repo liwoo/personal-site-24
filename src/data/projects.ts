d8c4fdd1e87d267ea010888faa2f87ccf9400d28
Jeremiah Chienda <jeremiahchienda@gmail.com>
2026-06-22 12:04:24 +0100

    made project names more realistic

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -196,11 +196,11 @@ export const projects: Project[] = [
       },
     ],
     testimony: {
-      name: 'Madalitso Gondwe',
+      name: 'Chisomo Gondwe',
       job: 'Operations Director',
       image: {
-        src: 'https://i.pravatar.cc/300?img=53',
-        alt: 'Madalitso Gondwe',
+        src: 'https://images.unsplash.com/photo-1771771425345-ea40b23e8220?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        alt: 'Chisomo Gondwe',
       },
       company: 'Gojet Investments',
       testimony:
@@ -250,11 +250,11 @@ export const projects: Project[] = [
       },
     ],
     testimony: {
-      name: 'Tamanda Biliate',
+      name: 'Gergio Tarkowksy',
       job: 'Software Engineer',
       image: {
         src: 'https://i.pravatar.cc/300?img=12',
-        alt: 'Tamanda Biliate',
+        alt: 'Gergio Tarkowksy',
       },
       company: 'Lilongwe, Malawi',
       testimony:
